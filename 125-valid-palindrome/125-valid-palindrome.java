ee58f80ba28068c3dfe0e88b99a90d51f559fa62
PritamSalunkhe1 <84986018+PritamSalunkhe1@users.noreply.github.com>
2022-07-18 21:32:09 +0530

    Time: 1713 ms (5.01%), Space: 406.6 MB (10.14%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,23 +1,15 @@
-
 class Solution {
+
     public boolean isPalindrome(String s) {
-        
-       
-        s = s.replaceAll("[^A-Za-z0-9]", ""); 
+        s = s.replaceAll("[^A-Za-z0-9]", "");
         s = s.toLowerCase();
-        
+
         String rev = "";
-        
-        for(int i = s.length()-1 ; i>=0; i--){
+
+        for (int i = s.length() - 1; i >= 0; i--) {
             rev += s.charAt(i);
-            
         }
-        
+
         return s.equals(rev);
-        }
-        
-    
-    
-    
     }
-    
+}
