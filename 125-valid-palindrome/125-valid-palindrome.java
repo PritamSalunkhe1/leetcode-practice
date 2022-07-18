47c7d3f3761ac62b1deb13d216e01d696aa4ca43
PritamSalunkhe1 <84986018+PritamSalunkhe1@users.noreply.github.com>
2022-07-18 21:30:45 +0530

    Time: 1839 ms (5.01%), Space: 473.4 MB (7.47%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+       
+        s = s.replaceAll("[^A-Za-z0-9]", ""); 
+        s = s.toLowerCase();
+        
+        String rev = "";
+        
+        for(int i = s.length()-1 ; i>=0; i--){
+            rev += s.charAt(i);
+            
+        }
+        
+        return s.equals(rev);
+        }
+        
+    
+    
+    
+    }
+    
