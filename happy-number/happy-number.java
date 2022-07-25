7acba997d48a359ce4b108f133c32a47330aae53
PritamSalunkhe1 <84986018+PritamSalunkhe1@users.noreply.github.com>
2022-07-25 11:40:08 +0530

    Time: 1 ms, Memory: 39.4 MB - LeetHub

--- happy-number/happy-number.java
+++ happy-number/happy-number.java
@@ -1,47 +1,26 @@
 class Solution {
-    
-    
-    
-     public static int numSquare(int n){
+
+    public static int numSquare(int n) {
         int sq = 0;
-        while(n!=0){
-            sq += (n%10) * (n%10);
-            n/=10;
+        while (n != 0) {
+            sq += (n % 10) * (n % 10);
+            n /= 10;
         }
         return sq;
     }
-    
-    
-    
+
     public boolean isHappy(int n) {
-        
-        
         HashSet<Integer> set = new HashSet<>();
-        
-        while(true){
+
+        while (true) {
             n = numSquare(n);
-            if(n==1){
+            if (n == 1) {
                 return true;
             }
-            if(set.contains(n)){
+            if (set.contains(n)) {
                 return false;
             }
             set.add(n);
         }
-        
-        
-        
-        
-        
-        
     }
-    
-    
-    
-    
-   
-    
-    
-    
-    
 }
