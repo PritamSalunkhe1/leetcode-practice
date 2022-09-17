0ba26511823f53ec43dd0838956480c9093fb9ad
PritamSalunkhe1 <84986018+PritamSalunkhe1@users.noreply.github.com>
2022-09-17 11:19:51 +0530

    Time: 1019 ms (5.07%), Space: 45.1 MB (31.01%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,19 +3,22 @@ import java.util.ArrayList;
 class Solution {
 
     public boolean isAnagram(String s, String t) {
+        //if length not equal, strings wont be pangrams
+
         if (s.length() != t.length()) {
             return false;
         }
+        // declare arraylist of chars , added all elements of one string
 
         ArrayList<Character> arr = new ArrayList<>();
         for (int i = 0; i < t.length(); i++) {
             arr.add(t.charAt(i));
         }
-        for (int i = 0; i < s.length(); i++) 
-            if (arr.contains(s.charAt(i))) {
+        //removed elements from arrlist that are present in second string.
+        for (int i = 0; i < s.length(); i++) if (arr.contains(s.charAt(i))) {
             arr.remove((Character.valueOf(s.charAt(i))));
         } else return false;
-
+        //if the arrlist will be empty, it will mean that all elements of s were present in t, hence we'll return
         return arr.isEmpty();
     }
 }
